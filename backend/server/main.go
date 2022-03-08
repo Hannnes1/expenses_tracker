b9561d04998fd79730b70426bb61fbb2077d53f2
Hannes Hultergård <hannes.hultergard@gmail.com>
2022-03-08 15:10:44 +0100

    Move validation code to main and change dependency

--- backend/server/main.go
+++ backend/server/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"database/sql"
 	"log"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/validator"
+	"github.com/go-playground/validator/v10"
 	"github.com/go-sql-driver/mysql"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
@@ -14,6 +15,12 @@ import (
 	"hultergard.com/expenses_tracker/docs"
 	"hultergard.com/expenses_tracker/repository"
 )
+
+var iso8601 validator.Func = func(fl validator.FieldLevel) bool {
+	ISO8601DateRegexString := "^(?:[1-9]\\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d(?:\\.\\d{1,9})?(?:Z|[+-][01]\\d:[0-5]\\d)$"
+	ISO8601DateRegex := regexp.MustCompile(ISO8601DateRegexString)
+	return ISO8601DateRegex.MatchString(fl.Field().String())
+}
 
 // @title    Expenses Tracker API
 // @version  0.1
